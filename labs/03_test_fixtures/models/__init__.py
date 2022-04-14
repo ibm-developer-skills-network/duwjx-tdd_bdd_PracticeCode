9ab1e643dc60ff94bbe5df71bfada6d089bdebac
John Rofrano <johnnyroy@johnrofrano.com>
2022-04-14 16:37:48 -0400

    Initial Lab 3

---
+++ labs/03_test_fixtures/models/__init__.py
@@ -0,0 +1,10 @@
+"""
+Data Models
+"""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
