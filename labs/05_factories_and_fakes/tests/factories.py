250e541de94623f2837818364346e6397faede02
John Rofrano <johnnyroy@johnrofrano.com>
2022-04-17 08:27:11 -0400

    Modified factory to match lecture

--- labs/05_factories_and_fakes/tests/factories.py
+++ labs/05_factories_and_fakes/tests/factories.py
@@ -1,10 +1,12 @@
+import factory
 from datetime import date
-from factory import Factory, Sequence, Faker, LazyFunction
 from factory.fuzzy import FuzzyChoice, FuzzyDate
 from models.account import Account
 
-class AccountFactory(Factory):
+class AccountFactory(factory.Factory):
     """ Creates fake Accounts """
 
     class Meta:
         model = Account
+
+    # Add attributes here...
