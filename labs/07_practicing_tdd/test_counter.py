3e101a24dcdc57926d79190936a37491bee415a4
John Rofrano <johnnyroy@johnrofrano.com>
2022-05-02 08:51:34 -0400

    Initial start

---
+++ labs/07_practicing_tdd/test_counter.py
@@ -0,0 +1,10 @@
+"""
+Test Cases for Counter Web Service
+
+Create a service that can keep a track of multiple counters
+ - API must be RESTful
+ - The endpoint should be called /counters
+ - When creating a counter, you specify the name in the path
+ - If the name being created already exists return a 409 Conflict
+
+"""
