69d7c7c018cc9dc222d46c07ce15d4e5648281e5
John Rofrano <johnnyroy@johnrofrano.com>
2022-05-19 18:41:05 +0000

    Initial setup

---
+++ labs/08_environment_setup/features/environment.py
@@ -0,0 +1,13 @@
+"""
+Environment for Behave Testing
+"""
+from os import getenv
+from selenium import webdriver
+
+
+def before_all(context):
+    """ Executed once before all tests """
+
+
+def after_all(context):
+    """ Executed after all tests """
