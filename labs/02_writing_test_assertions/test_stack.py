7fd9657c9547b4263dbd0722cbdab7943520d6cd
Agiopoulos Kaptsikas <agiopoulos@outlook.com.gr>
2023-07-24 19:28:31 +0300

    new file:   .vscode/settings.json 	modified:   labs/02_writing_test_assertions/test_stack.py

--- labs/02_writing_test_assertions/test_stack.py
+++ labs/02_writing_test_assertions/test_stack.py
@@ -14,16 +14,26 @@ class TestStack(TestCase):
 
     def test_push(self):
         """Test pushing an item into the stack"""
-        raise Exception("not implemented")
+        self.stack.push(3)
+        self.assertEquals(self.stack.peek(), 3)
+        self.stack.push(5)
+        self.assertTrue(self.stack.pop(), 5)
 
     def test_pop(self):
         """Test popping an item of off the stack"""
-        raise Exception("not implemented")
+        self.stack.push(3)
+        self.stack.push(5)
+        self.assertEquals(self.stack.pop(), 5)
+        self.assertEquals(self.stack.peek(), 3)
 
     def test_peek(self):
         """Test peeking at the top the stack"""
-        raise Exception("not implemented")
+        self.stack.push(3)
+        self.stack.push(5)
+        self.assertEquals(self.stack.peek(), 5)
 
     def test_is_empty(self):
         """Test if the stack is empty"""
-        raise Exception("not implemented")
+        self.assertTrue(self.stack.is_empty())
+        self.stack.push(3)
+        self.assertFalse(self.stack.is_empty())
