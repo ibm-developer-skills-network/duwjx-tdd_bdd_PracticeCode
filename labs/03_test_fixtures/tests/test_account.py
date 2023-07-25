5f630bc9f040830b97897ae5b60a5222470f9d25
Agiopoulos Kaptsikas <agiopoulos@outlook.com.gr>
2023-07-25 12:14:16 +0300

    modified:   labs/03_test_fixtures/models/__init__.py 	modified:   labs/03_test_fixtures/models/account.py 	modified:   labs/03_test_fixtures/tests/test_account.py

--- labs/03_test_fixtures/tests/test_account.py
+++ labs/03_test_fixtures/tests/test_account.py
@@ -14,13 +14,20 @@ class TestAccountModel(TestCase):
     @classmethod
     def setUpClass(cls):
         """ Connect and load data needed by tests """
+        db.create_all()
+
+        global ACCOUNT_DATA
+        with open('tests/fixtures/account_data.json') as json_data:
+            ACCOUNT_DATA = json.load(json_data)
 
     @classmethod
     def tearDownClass(cls):
         """Disconnect from database"""
+        db.session.close()
 
     def setUp(self):
         """Truncate the tables"""
+        db.session.query(Account).delete()
 
     def tearDown(self):
         """Remove the session"""
@@ -29,3 +36,15 @@ class TestAccountModel(TestCase):
     #  T E S T   C A S E S
     ######################################################################
 
+    def test_create_an_account(self):
+        """Test creating an account"""
+        account = Account(**ACCOUNT_DATA[0])
+        account.create()
+        self.assertEqual(len(Account.all()), 1)
+
+    def test_create_all_accounts(self):
+        """Test creating all accounts"""
+        for account_data in ACCOUNT_DATA:
+            account = Account(**account_data)
+            account.create()
+        self.assertEqual(len(Account.all()), len(ACCOUNT_DATA))
