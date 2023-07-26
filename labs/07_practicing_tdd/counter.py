7f091bcf09db8746cb2d0b19e36215367724f68b
Agiopoulos Kaptsikas <agiopoulos@outlook.com.gr>
2023-07-26 21:30:43 +0300

    modified:   labs/07_practicing_tdd/counter.py 	modified:   labs/07_practicing_tdd/test_counter.py

--- labs/07_practicing_tdd/counter.py
+++ labs/07_practicing_tdd/counter.py
@@ -16,3 +16,28 @@ def create_counter(name):
 
     COUNTERS[name] = 0
     return { name: COUNTERS[name] }, status.HTTP_201_CREATED
+
+@app.route("/counters/<name>", methods=["PUT"])
+def update_counter(name):
+    """Updates a counter"""
+    app.logger.info(f"Request to update counter: {name}")
+    global COUNTERS
+    COUNTERS[name] += 1
+    app.logger.info(f"Counter: {name} is now {COUNTERS[name]}")
+    return { name: COUNTERS[name] }, status.HTTP_200_OK
+
+@app.route("/counters/<name>", methods=["GET"])
+def read_counter(name):
+    """Reads a counter"""
+    app.logger.info(f"Request to read counter: {name}")
+    app.logger.info(f"Counter: {name} is now {COUNTERS[name]}")
+    return { name: COUNTERS[name] }, status.HTTP_200_OK
+
+@app.route("/counters/<name>", methods=["DELETE"])
+def delete_counter(name):
+    """Deletes a counter"""
+    app.logger.info(f"Request to delete counter: {name}")
+    global COUNTERS
+    del COUNTERS[name]
+    app.logger.info(f"Counter: {name} has been deleted")
+    return "", status.HTTP_204_NO_CONTENT
--- labs/07_practicing_tdd/test_counter.py
+++ labs/07_practicing_tdd/test_counter.py
@@ -25,3 +25,30 @@ class CounterTest(TestCase):
         self.assertEqual(result.status_code, status.HTTP_201_CREATED)
         result = self.client.post("/counters/bar")
         self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_update_a_counter(self):
+        """It should update a counter"""
+        result = self.client.post("/counters/cow")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        data = result.get_json()
+        baseline = data["cow"]
+        result = self.client.put("/counters/cow")
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        data = result.get_json()
+        self.assertEqual(data["cow"], baseline + 1)
+
+    def test_read_a_counter(self):
+        """It should read a counter"""
+        result = self.client.post("/counters/gif")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.get("/counters/gif")
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        data = result.get_json()
+        self.assertEqual(data["gif"], 0)
+
+    def test_delete_a_counter(self):
+        """It should delete a counter"""
+        result = self.client.post("/counters/to_be_deleted")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.delete("counters/to_be_deleted")
+        self.assertEqual(result.status_code, status.HTTP_204_NO_CONTENT)
