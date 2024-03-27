0afe2d30d978e016dfd3b82da6f15e773cd619ad
John Rofrano <johnnyroy@johnrofrano.com>
2024-03-27 13:38:46 +0000

    Made Python 3.8 the default

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,10 +4,10 @@ echo " Setting up BDD Environment"
 echo "****************************************"
 
 echo "Making Python 3.8 the default..."
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 
 echo "Checking the Python version..."
-python3 --version
+python --version
 
 echo "Configuring the developer environment..."
 echo "# BDD Lab Additions" >> ~/.bashrc
